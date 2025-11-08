923dfbc6d163106cd1311d36da1f7c161aa09b7c
Rahul Singh <rahulcodes1628@gmail.com>
2025-11-09 01:48:56 +0530

    Initial commit - Spring Boot setup

---
+++ src/main/java/com/coachme/coachme/CoachmeApplication.java
@@ -0,0 +1,13 @@
+package com.coachme.coachme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachmeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/coachme/coachme/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.coachme.coachme.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String sayHello(){
+        return "Welcome to Coach Me! Your Learning journey starts here";
+    }
+
+}
